a14ae370eb3006d49f24b1aa08627ce8452dc4f6
Hemant Upadhyay <hemantupadhyay59@gmail.com>
2019-12-03 10:05:44 -0800

    initial commit of SPFx webpart

---
+++ src/webparts/myFisrtWp/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IMyFisrtWpWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'MyFisrtWpWebPartStrings' {
+  const strings: IMyFisrtWpWebPartStrings;
+  export = strings;
+}
